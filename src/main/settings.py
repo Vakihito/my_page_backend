d724439bc32bac8da9b9fafb25d2822949de3aae
Victor Akihito Kamada Tomita <40372914+Vakihito@users.noreply.github.com>
2024-05-22 15:38:23 +0000

    fix settings

--- src/main/settings.py
+++ src/main/settings.py
@@ -3,8 +3,8 @@ from functools import lru_cache
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:postgres@0.0.0.0:5433/postgres"
-    TEST_DATABASE_URL: str = "postgresql://postgres:postgres@0.0.0.0:5433/postgres_test"
+    DATABASE_URL: str = "postgresql://postgres:postgres@postgres:5432/postgres"
+    TEST_DATABASE_URL: str = "postgresql://postgres:postgres@postgres:5432/postgres_test"
     LOG_LEVEL: str = "INFO"
     LOG_LEVEL_DB: str = "ERROR"
     TESTING: bool = False
