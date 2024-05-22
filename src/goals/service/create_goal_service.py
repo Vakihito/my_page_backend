32b25e3c98744aba93eb0fabc9126f94a30dc525
Victor Akihito Kamada Tomita <40372914+Vakihito@users.noreply.github.com>
2024-05-22 03:01:14 +0000

    added service

--- src/goals/service/create_goal_service.py
+++ src/goals/service/create_goal_service.py
@@ -1,12 +1,22 @@
 from src.goals.schema import CreateGoalInputSchema, CreateGoalResponseSchema
+from src.goals.model import GoalsModel
+from src.goals.infra import GoalsRepository
 
 
 class CreateGoalService:
-    def __init__(self) -> CreateGoalResponseSchema:
-        self.repo = None
+    def __init__(
+        self, create_goal_repository: GoalsRepository
+    ) -> CreateGoalResponseSchema:
+        self.create_goal_repository = create_goal_repository
 
     def create(
         self, create_goal_input: CreateGoalInputSchema
     ) -> CreateGoalResponseSchema:
-        create_goal_reseponse = {"created": True}
+        create_goal_reseponse = self.create_goal_repository.create_goal(
+            create_goal_input
+        )
+        if create_goal_reseponse is not None:
+            create_goal_reseponse = {"created": True}
+        else:
+            create_goal_reseponse = {"created": False}
         return create_goal_reseponse
