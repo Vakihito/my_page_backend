611fb48777511c9e0c47ad1af9895de0b631ff09
Victor Akihito Kamada Tomita <40372914+Vakihito@users.noreply.github.com>
2024-05-22 15:52:12 +0000

    fix factory

--- src/goals/factory/create_goal_factory.py
+++ src/goals/factory/create_goal_factory.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 from src.goals.service import CreateGoalService
 from src.goals.controller import CreateGoalController
+from src.goals.infra import GoalsRepository
+from src.shared.database_shared import get_db_session
 
+db = next(get_db_session())
 
 def create_goal_router_factory() -> APIRouter:
-    service = CreateGoalService()
+    repository = GoalsRepository(db)
+    service = CreateGoalService(repository)
     controller = CreateGoalController(service)
     return controller.router
--- src/goals/schema/goals.py
+++ src/goals/schema/goals.py
@@ -1,11 +1,14 @@
 from uuid import UUID
 from datetime import date, datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class CreateGoalInputSchema(BaseModel):
     id: UUID
     title: str
+    model_config = ConfigDict(id="00000000-0000-0000-0000-000000000000",
+                title="cool little text",
+            )
 
 
 class CreateGoalResponseSchema(BaseModel):
